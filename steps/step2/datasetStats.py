44be95599897e4325a63ce16cfefe336e4fa253b
AsherIDE <asher.de.jong@student.uva.nl>
2024-09-10 16:19:02 +0200

    Start of 2.2

---
+++ steps/step2/datasetStats.py
@@ -0,0 +1,16 @@
+import plotly.express as px
+import pandas as pd
+
+############################################################################################
+# TODO TODO (step 2.2)
+############################################################################################
+
+# input -> class name, outliers sd threshold, visualize faces and/or vertices
+# output -> 
+#        - average shape (faces, vertices)
+#        - outliers (faces, vertices)
+#        - obj class
+# idee: gebruik kleuren om outliers te markeren
+
+def get_class_barplot(class_name, sd=2, type="f"):
+    print("hello world")
