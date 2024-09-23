92c555225374a2f941474b3f4c11bfd8f7f304f6
GingerWars <axelvanburen2001@gmail.com>
2024-09-24 01:07:32 +0200

    Created a newer object loader that uses the other python file for calculations of descriptors. Big starts but long way to go still. Step 3.2

---
+++ steps/step3/objLoaderV3.py
@@ -0,0 +1,344 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QLineEdit,QSpacerItem, QHBoxLayout, QLabel, QSizePolicy
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtOpenGL import QGLWidget
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+from objectCalculator import ObjectCalculations  # Import the class from the other file
+
+
+############################################################################################
+# OpenGL widget
+############################################################################################
+class OpenGLWidget(QGLWidget):
+    def __init__(self, parent=None):
+        super(OpenGLWidget, self).__init__(parent)
+        self.vertices = []
+        self.faces = []
+        self.display_faces = False
+        self.display_edges = False
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateGL)
+        self.timer.start(16)  # 16 is near 60 fps
+        #voor de camera
+        self.camera_pos = np.array([0.0, 0.0, -5.0])  # X, Y, Z position
+        self.mouse_last_pos = None
+        self.is_right_button_pressed = False  # Initialize the flag here 
+        self.is_left_button_pressed = False #Left button standard false 
+        # Object rotation angles
+        self.rotation_x = 0
+        self.rotation_y = 0
+        #ObjectCalculator
+        
+        
+        
+############################################################################################
+# Loading and reading the object file
+############################################################################################
+    def load_obj_file(self, fileName):
+        self.vertices = []
+        self.faces = []
+        with open(fileName, 'r') as file:
+            for line in file:
+                if line.startswith('v '):
+                    self.vertices.append(list(map(float, line.strip().split()[1:])))
+                elif line.startswith('f '):
+                    self.faces.append([int(idx.split('/')[0]) - 1 for idx in line.strip().split()[1:]])
+        self.update()      
+
+############################################################################################
+# Rendering the objects *Lights in progress*
+############################################################################################
+    def initializeGL(self):
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA) 
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_COLOR_MATERIAL)
+        glLightfv(GL_LIGHT0, GL_POSITION, [1, 1, 1, 0])
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, [0.8, 0.8, 0.8, 1.0])
+        glLightfv(GL_LIGHT0, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
+        glEnable(GL_LIGHT1)
+        glLightfv(GL_LIGHT1, GL_POSITION, [-1, -1, -1, 0])
+        glLightfv(GL_LIGHT1, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, [0.8, 0.8, 0.8, 1.0])
+        glLightfv(GL_LIGHT1, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, [0.5, 0.5, 0.5, 1.0])
+        glMaterialfv(GL_FRONT, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
+        glMaterialf(GL_FRONT, GL_SHININESS, 50)
+        glMatrixMode(GL_PROJECTION)
+        gluPerspective(45, 1.33, 0.1, 50.0)
+        glMatrixMode(GL_MODELVIEW)
+        glTranslatef(0.0, 0.0, -5)
+
+    #Painting the background
+    def paintGL(self):
+        glClearColor(0.878, 1.000, 1.000, 0.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        # Apply camera translation before rendering the object
+        glLoadIdentity()  # Reset transformations
+        glTranslatef(self.camera_pos[0], self.camera_pos[1], self.camera_pos[2]) 
+
+        # Apply object rotation based on mouse input
+        glRotatef(self.rotation_x, 1, 0, 0)  # Rotate around X-axis
+        glRotatef(self.rotation_y, 0, 1, 0)  # Rotate around Y-axis
+        self.draw_vertices()
+        if self.display_faces:                    
+            self.draw_faces()
+        if self.display_edges:
+            self.draw_edges() #voor nu nog even niet
+
+    #Drawing the vertices   
+    def draw_vertices(self):
+        glBegin(GL_POINTS)
+        glColor3f(1.0, 0.0, 1.0)
+        for vertex in self.vertices:
+            glVertex3fv(vertex)
+        glEnd()
+
+    # Drawing the edges
+    def draw_edges(self):
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        glEnable(GL_POLYGON_OFFSET_LINE)
+        glPolygonOffset(-1.0, -1.0)
+        glColor3f(0.0, 0.0, 0.0)  # Black color for edges
+        glBegin(GL_TRIANGLES)
+        for face in self.faces:
+            for vertex_idx in face:
+                glVertex3fv(self.vertices[vertex_idx])
+        glEnd()
+        glDisable(GL_POLYGON_OFFSET_LINE)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
+    #Drawing the faces
+    def draw_faces(self):
+        glBegin(GL_TRIANGLES)
+        glColor4f(0.867, 0.627, 0.867, 0.8)
+        for face in self.faces:
+            for vertex_idx in face:
+                glVertex3fv(self.vertices[vertex_idx])
+        glEnd()
+
+    #Toggle view
+    def toggle_display_mode(self):
+        self.display_faces = not self.display_faces
+        self.update()
+
+    def toggle_edges(self):
+        self.display_edges = not self.display_edges
+        self.update()
+
+    #Getting the counts
+    def get_vertices_count(self):
+        return len(self.vertices)
+
+    def get_faces_count(self):
+        return len(self.faces)
+
+
+############################################################################################
+# Adding the mouse events
+############################################################################################
+    #Detecting pressed button (right)
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            self.is_right_button_pressed = True
+            self.mouse_last_pos = event.pos()
+        elif event.button() == Qt.LeftButton:
+            self.is_left_button_pressed = True
+            self.mouse_last_pos = event.pos()
+
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.RightButton:
+            self.is_right_button_pressed = False
+        elif event.button() == Qt.LeftButton:
+            self.is_left_button_pressed = False
+
+    # Mouse move event to translate camera along X and Y axes
+    def mouseMoveEvent(self, event):
+        if self.is_right_button_pressed:
+            dx = event.x() - self.mouse_last_pos.x()
+            dy = event.y() - self.mouse_last_pos.y()
+
+            # Sensitivity factor for camera movement
+            sensitivity = 0.01
+
+            # Update camera position along the X and Y axes based on mouse movement
+            self.camera_pos[0] -= dx * sensitivity  # Move along X axis
+            self.camera_pos[1] += dy * sensitivity  # Move along Y axis (inverted)
+
+            self.mouse_last_pos = event.pos()  # Update the last mouse position
+
+        elif self.is_left_button_pressed:
+            dx = event.x() - self.mouse_last_pos.x()
+            dy = event.y() - self.mouse_last_pos.y() 
+
+             # Sensitivity for rotation
+            rotation_sensitivity = 0.5
+
+            # Update rotation angles for object
+            self.rotation_x += dy * rotation_sensitivity  # Rotate around X-axis (up/down)
+            self.rotation_y += dx * rotation_sensitivity  # Rotate around Y-axis (left/right)
+
+            self.mouse_last_pos = event.pos()  # Update the last mouse position
+
+    def wheelEvent(self, event):
+        zoom_sensitivity = 0.35  # Adjust sensitivity as needed
+        delta = event.angleDelta().y()  # Get the scroll amount
+    
+        if delta > 0:
+            self.camera_pos[2] += zoom_sensitivity  # Zoom out
+        else:
+            self.camera_pos[2] -= zoom_sensitivity  # Zoom in    
+        self.update()  # Update the view
+
+
+############################################################################################
+# Window (in which OpenGL widget exists)
+############################################################################################
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Object testing')
+        self.setGeometry(100, 100, 900, 600)
+        self.opengl_widget = OpenGLWidget(self)
+        self.shape = None
+    
+        # Create side panel with buttons
+        self.side_panel = QWidget(self)
+        self.side_layout = QVBoxLayout(self.side_panel)
+        self.side_panel.setFixedWidth(240)
+        
+        # Open file button
+        self.button = QPushButton('Open OBJ File', self)
+        self.button.clicked.connect(self.open_file_dialog)
+        self.side_layout.addWidget(self.button)
+        
+        # Text box to display chosen file
+        self.file_label = QLabel('The loaded file:', self)
+        self.side_layout.addWidget(self.file_label)
+        self.file_display = QLineEdit(self)
+        self.file_display.setReadOnly(True)
+        self.side_layout.addWidget(self.file_display)
+        self.side_layout.addSpacerItem(QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Fixed))
+        
+        # Toggle button faces/vertices
+        self.toggle_button = QPushButton('Toggle vertices/faces', self)
+        self.toggle_button.clicked.connect(self.opengl_widget.toggle_display_mode)
+        self.side_layout.addWidget(self.toggle_button)
+
+        # Toggle button edges
+        self.toggle_button_edges = QPushButton('Toggle edges; causes lag for high F-count', self)
+        self.toggle_button_edges.clicked.connect(self.opengl_widget.toggle_edges)
+        self.side_layout.addWidget(self.toggle_button_edges)
+
+        # Text boxes to display vertices and faces count
+        self.vertices_label = QLabel('Vertices count:', self)
+        self.side_layout.addWidget(self.vertices_label)
+        self.vertices_display = QLineEdit(self)
+        self.vertices_display.setReadOnly(True)
+        self.side_layout.addWidget(self.vertices_display)
+        
+        self.faces_label = QLabel('Faces count:', self)
+        self.side_layout.addWidget(self.faces_label)
+        self.faces_display = QLineEdit(self)
+        self.faces_display.setReadOnly(True)
+        self.side_layout.addWidget(self.faces_display)
+
+        # Main layout + spacing the buttons
+        self.side_layout.addStretch()
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.opengl_widget, 3)  # Stretch factor to make OpenGL widget larger
+        main_layout.addWidget(self.side_panel)     # Stretch factor to make side panel smaller 
+
+        # Bottom panel for object calculations
+        self.bottom_panel = QWidget(self)
+        self.bottom_layout = QHBoxLayout(self.bottom_panel)
+        
+        self.surfaceArea_display = QLabel('Surface Area')
+        self.bottom_layout.addWidget(self.surfaceArea_display)
+        
+        self.compactness_display = QLabel('Compactness')
+        self.bottom_layout.addWidget(self.compactness_display)
+        
+        self.rectangularity_display = QLabel('3D Rectangularity')
+        self.bottom_layout.addWidget(self.rectangularity_display)
+        
+        self.diameter_display = QLabel('Diameter')
+        self.bottom_layout.addWidget(self.diameter_display)
+        
+        self.convexity_display = QLabel('Convexity')
+        self.bottom_layout.addWidget(self.convexity_display)
+
+        self.eccentricity_display = QLabel('Eccentricity')
+        self.bottom_layout.addWidget(self.eccentricity_display)
+
+        labels = [
+            self.surfaceArea_display,
+            self.compactness_display,
+            self.rectangularity_display,
+            self.diameter_display,
+            self.convexity_display,
+            self.eccentricity_display
+        ]
+        
+        for label in labels:
+            label.setStyleSheet("border: 1px solid black; padding: 5px;")
+            self.bottom_layout.addWidget(label)
+        
+        # Add bottom panel to main layout
+        main_layout_with_bottom = QVBoxLayout()
+        main_layout_with_bottom.addLayout(main_layout)
+        main_layout_with_bottom.addWidget(self.bottom_panel)
+        
+        container = QWidget()
+        container.setLayout(main_layout_with_bottom)
+        self.setCentralWidget(container)
+
+    def calcSurfaceArea(self):
+        self.surfaceArea_display.setText(f'Surface Area: {self.shape.surfaceArea}')
+
+    def calcCompactness(self):
+        self.compactness_display.setText(f'Compactness: {self.shape.objCompactness}')
+
+    def calcRectangularity(self):
+        self.rectangularity_display.setText(f'3D Rectangularity: {self.shape.rectangularity()}')
+
+    def calcDiameter(self):
+        self.diameter_display.setText('Diameter:Breaks')
+
+    def calcConvexity(self):
+        self.convexity_display.setText(f'Convexity: {self.shape.convexity()}')
+
+    def calcEccentricity(self):
+        self.eccentricity_display.setText(f'Eccentricity: {self.shape.eccentricity()}')
+
+    #Function for opening the file and setting all stats according to the file
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open OBJ File", "", "OBJ Files (*.obj);;All Files (*)", options=options)
+        if fileName:
+            self.opengl_widget.load_obj_file(fileName)
+            self.file_display.setText(fileName)
+            self.vertices_display.setText(str(self.opengl_widget.get_vertices_count()))
+            self.faces_display.setText(str(self.opengl_widget.get_faces_count()))
+            self.shape = ObjectCalculations(fileName)  # Assuming you have a method to load the shape
+            self.calcSurfaceArea()
+            self.calcCompactness()
+            self.calcRectangularity()
+            self.calcDiameter()
+            self.calcConvexity()
+            self.calcEccentricity()
+
+# execution of the program
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ steps/step3/objectCalculator.py
@@ -0,0 +1,94 @@
+import numpy as np
+from scipy.spatial import ConvexHull
+from math import pi
+
+class ObjectCalculations:
+    def __init__(self, obj_file):
+        self.vertices, self.faces, self.SurfaceArea, self.volume = self.load_obj(obj_file)
+        self.surfaceArea = self.calcSurfaceArea()
+        self.objCompactness = self.calcCompactness()
+        self.convex_hull_volume = self.calculate_convex_hull_volume()
+        self.eigenvalues = self.calculate_eigenvalues()
+        self.obb_volume = self.calculate_obb_volume()
+
+    def load_obj(self, obj_file):
+        vertices = []
+        faces = []
+        SurfaceArea = 0.0
+        volume = 0.0
+        with open(obj_file, 'r') as file:
+            for line in file:
+                if line.startswith('v '):
+                    vertices.append(list(map(float, line.strip().split()[1:])))
+                elif line.startswith('f '):
+                    face = [int(idx.split('/')[0]) - 1 for idx in line.strip().split()[1:]]
+                    faces.append(face)
+                    # Calculate surface area and volume contribution of this face
+                    v0, v1, v2 = [vertices[idx] for idx in face]
+                    # Surface area calculation
+                    triangle_area = np.linalg.norm(np.cross(np.array(v1) - np.array(v0), np.array(v2) - np.array(v0))) / 2
+                    SurfaceArea += triangle_area
+                    # Volume calculation using the signed volume of the tetrahedron
+                    volume += np.dot(v0, np.cross(v1, v2)) / 6.0
+        return np.array(vertices), np.array(faces), SurfaceArea, abs(volume)
+
+    def calcSurfaceArea(self):
+        return float(f"{self.SurfaceArea:.5f}")
+
+    def calcCompactness(self):
+        sphere_radius = (self.SurfaceArea / (4 * pi)) ** 0.5
+        sphere_volume = (4/3) * pi * (sphere_radius ** 3)
+        compactness = self.volume/sphere_volume
+        return float(f"{compactness:.5f}")
+
+    def calculate_convex_hull_volume(self):
+        hull = ConvexHull(self.vertices)
+        return hull.volume
+
+    def calculate_eigenvalues(self):
+        cov_matrix = np.cov(self.vertices.T)
+        eigenvalues, _ = np.linalg.eig(cov_matrix)
+        return eigenvalues
+
+    def compactness(self):
+        return (self.volume ** 2) / (self.SurfaceArea ** 3)
+
+    def rectangularity(self):
+        return float(f"{self.volume / self.obb_volume:.5f}")
+
+    def diameter(self):
+        max_distance = 0
+        for i in range(len(self.vertices)):
+            for j in range(i + 1, len(self.vertices)):
+                distance = np.linalg.norm(self.vertices[i] - self.vertices[j])
+                if distance > max_distance:
+                    max_distance = distance
+        return max_distance
+
+    def convexity(self):
+        return float(f"{self.volume / self.convex_hull_volume :.5f}")
+
+    def eccentricity(self):
+        return float(f"{(max(self.eigenvalues) / min(self.eigenvalues)):.5f}")
+    
+    def calculate_obb_volume(self):
+        # Step 1: Compute the covariance matrix
+        mean = np.mean(self.vertices, axis=0)
+        centered_points = self.vertices - mean
+        covariance_matrix = np.cov(centered_points, rowvar=False)
+        
+        # Step 2: Eigen decomposition
+        eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
+        
+        # Step 3: Rotate the points to align with the principal axes
+        rotated_points = np.dot(centered_points, eigenvectors)
+        
+        # Step 4: Find the extents
+        min_extents = np.min(rotated_points, axis=0)
+        max_extents = np.max(rotated_points, axis=0)
+        
+        # Step 5: Calculate the volume of the OBB
+        obb_extents = max_extents - min_extents
+        obb_volume = np.prod(obb_extents)
+        
+        return obb_volume
